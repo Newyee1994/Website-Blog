44e26dea2ad24c380e355d4059034fb249c4cb40
Newyee1994 <n799230342y@qq.com>
2021-05-19 23:33:35 +0800

    新用法

--- www/app.py
+++ www/app.py
@@ -5,25 +5,20 @@ async web application.
 """
 
 import logging; logging.basicConfig(level=logging.INFO)
-import asyncio
 from aiohttp import web
+# import asyncio
 
 
-def index(request):
+async def index(request):
     return web.Response(body=b'<h1>Awesome Website</h1>', content_type='text/html')
 
 
-async def init(loop):
+def init():
     app = web.Application()
-    app.router.add_route('GET', '/', index)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    srv = await loop.create_server(runner.server, host='127.0.0.1', port=9000)
-    logging.info('server started at http://127.0.0.1:9000...')
-    return srv
+    app.router.add_get('/', index)
+    logging.info('Server started at http://127.0.0.1:9000...')
+    web.run_app(app, host='127.0.0.1', port=9000)
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(init(loop))
-    loop.run_forever()
+    init()
