6cfe4887fad547f43da561e395054a4b007d6d2e
Newyee1994 <n799230342y@qq.com>
2021-05-24 22:02:19 +0800

    日志相关API

--- www/handlers.py
+++ www/handlers.py
@@ -13,6 +13,8 @@ import base64
 import asyncio
 from aiohttp import web
 
+from markdown2 import markdown
+
 from config import configs
 from coroweb import get, post
 from models import User, Comment, Blog, next_id
@@ -23,6 +25,30 @@ COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+
+def check_admin(request):
+    """ 检查是否是管理员用户 """
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissionError()
+
+
+def get_page_index(page_str):
+    """ 获取页码信息 """
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
+
+
+def text2html(text):
+    """ 文本转HTML """
+    lines = map(lambda s: '<p>%s</p>' % s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;'), filter(lambda s: s.strip() != '', text.split('\n')))
+    return ''.join(lines)
 
 
 def user2cookie(user, max_page):
@@ -59,18 +85,6 @@ async def cookie2user(cookie_str: str):
         return None
 
 
-def get_page_index(page_str):
-    """ 获取页码信息 """
-    p = 1
-    try:
-        p = int(page_str)
-    except ValueError as e:
-        pass
-    if p < 1:
-        p = 1
-    return p
-
-
 @get('/')
 async def index(request):
     """ 处理首页URL """
@@ -89,6 +103,31 @@ async def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs,
         # '__user__': request.__user__
+    }
+
+
+@get('/blog/{id}')
+async def get_blog(id):
+    """ 处理日志详情页面URL """
+    blog = await Blog.find(id)
+    comments = await Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
+    for c in comments:
+        c.html_content = text2html(c.content)
+    blog.html_content = markdown(blog.content)
+    return {
+        '__template__': 'blog.html',
+        'blog': blog,
+        'comments': comments
+    }
+
+
+@get('/manage/blogs/create')
+def manage_create_blog():
+    """ 创建日志页面 """
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': '',
+        'action': '/api/blogs'
     }
 
 
@@ -187,3 +226,25 @@ async def api_get_users(*, page='1'):
     for u in users:
         u.passwd = '*' * 6
     return dict(page=p, users=users)
+
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*, id):
+    """ 获取日志详情API """
+    blog = await Blog.find(id)
+    return blog
+
+
+@post('/api/blogs')
+async def api_create_blog(request, *, name, summary, content):
+    """ 发表日志API """
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name', 'Name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'Summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content', 'Content cannot be empty.')
+    blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image, name=name.strip(), summary=summary.strip(), content=content.strip())
+    await blog.save()
+    return blog
