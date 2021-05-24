566579ee4799a7aad75b2baa5e491673de937192
Newyee1994 <n799230342y@qq.com>
2021-05-24 22:03:17 +0800

    更新 Page

--- www/apis.py
+++ www/apis.py
@@ -12,8 +12,33 @@ import functools
 
 
 class Page:
-    """ 建立Page类来处理分页,可以在page_size更改每页项目的个数 """
-    def __init__(self, item_count, page_index=1, page_size=8):
+    """ 建立Page类来处理分页,可以在page_size更改每页项目的个数：Page object for display pages. """
+    def __init__(self, item_count, page_index=1, page_size=10):
+        '''
+        Init Pagination by item_count, page_index and page_size.
+
+        >>> p1 = Page(100, 1)
+        >>> p1.page_count
+        10
+        >>> p1.offset
+        0
+        >>> p1.limit
+        10
+        >>> p2 = Page(90, 9, 10)
+        >>> p2.page_count
+        9
+        >>> p2.offset
+        80
+        >>> p2.limit
+        10
+        >>> p3 = Page(91, 10, 10)
+        >>> p3.page_count
+        10
+        >>> p3.offset
+        90
+        >>> p3.limit
+        10
+        '''
         self.item_count = item_count
         self.page_size = page_size
         self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
@@ -29,8 +54,7 @@ class Page:
         self.has_previous = self.page_index > 1
 
     def __str__(self):
-        return 'item_count: %s, page_count: %s, page_index: %s, page_size: %s, offset: %s, limit: %s' % \
-               (self.item_count, self.page_count, self.page_index, self.page_size, self.offset, self.limit)
+        return 'item_count: %s, page_count: %s, page_index: %s, page_size: %s, offset: %s, limit: %s' % (self.item_count, self.page_count, self.page_index, self.page_size, self.offset, self.limit)
 
     __repr__ = __str__
 
